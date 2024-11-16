4087d93b4644761798b857152784c0c3ba7161ed
DarkVexon <edanvirgo@gmail.com>
2024-11-16 12:19:55 -0700

    small

--- boxes/exp.gd
+++ boxes/exp.gd
@@ -1,12 +1,12 @@
 extends Box
 
 func on_open() -> void:
-    set_custom_num(9)
+	set_custom_num(9)
 
-func on_other_box_opened() -> void:
-    if open and !main.last_opened.was_revealed_when_opened and customNum > 0 and !just_opened:
-        set_custom_num(customNum-1)
-        if customNum == 0:
-            main.win()
-            if main.gameRunning:
-                hide_custom_num()
+func on_other_box_opened(other) -> void:
+	if open and !main.last_opened.was_revealed_when_opened and customNum > 0 and !just_opened:
+		set_custom_num(customNum-1)
+		if customNum == 0:
+			main.win()
+			if main.gameRunning:
+				hide_custom_num()
